78af8828475676b50cb53967be460b6856e0164f
whenrow <william.henrotin@gmail.com>
2015-08-15 14:37:14 +0200

    Test get timeline

--- Cartwit.py
+++ Cartwit.py
@@ -2,4 +2,19 @@
 
 # Import modules
 import twitter
+import json
 from accounts import *
+
+# Connection data
+app = account["twitter"]
+api = twitter.Api(consumer_key=app["api_key"],
+                  consumer_secret=app["api_secret"],
+                  access_token_key=app["token"],
+                  access_token_secret=app["token_secret"])
+
+
+tweets = api.GetUserTimeline("Whenrow")
+
+tweets = [tweet.AsDict() for tweet in tweets]
+with open('tweets.json', 'w') as outfile:
+        json.dump(tweets, outfile)
